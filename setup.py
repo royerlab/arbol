258f58c568e2fc4f7c2c7b0813d576a48e8ba6e4
Loic Royer <loic.royer@czbiohub.org>
2021-05-18 16:32:57 -0700

    fixed support for ansicolors on windows

--- setup.py
+++ setup.py
@@ -1,24 +1,144 @@
-import setuptools
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pipenv install twine --dev
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'arbol'
+DESCRIPTION = 'arbol -- Arborescent Printouts in Python'
+URL = 'https://github.com/royerlab/dexp'
+EMAIL = 'loic.royer@czbiohub.org'
+AUTHOR = 'Loic A Royer'
+REQUIRES_PYTHON = '>=3.6.0'
+
+from datetime import datetime
+now = datetime.now()
+seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+minutes_since_midnight = int(seconds_since_midnight//60)
+ten_minutes_since_midnight = int(seconds_since_midnight//600)
+VERSION = datetime.today().strftime('%Y.%m.%d')+f'.{minutes_since_midnight}'
+print(f"Version: {VERSION}")
+
+# What packages are required for this module to be executed?
+REQUIRED = []
+
+# What packages are optional?
+EXTRAS = {'color': ['colorama', 'ansicolors',]}
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
-setuptools.setup(
-    name="arbol", # Replace with your own username
-    version="2020.11.6",
-    author="Loic A Royer",
-    author_email="",
-    description="arbol -- Arborescent Printouts in Python",
+
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        # install twine if not
+        os.system('pip install -y twine --dev')
+
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status(f'Uploading version `{about["__version__"]}` of the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/royerlab/arbol",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='BSD 3-Clause',
+    license_file='LICENSE.txt',
     classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand
+    },
 )
+
+
+
+
 
